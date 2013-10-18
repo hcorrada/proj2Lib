d7bed29f603c2d075ebc7a0e8a2c4cb24c9a8f62
hcorrada <hcorrada@gmail.com>
2013-10-18 08:12:39 -0400

    KMPSetMatcher and Naive keyword tree matcher

--- ACSetMatcher.py
+++ ACSetMatcher.py
@@ -1,13 +1,34 @@
-from proj2Lib.ExactMatcher import ExactMatcher as ExactMatcher
-from proj2Lib.ExactMatcherNode import ExactMatcherNode as ExactMatcherNode
+from proj2Lib.ExactMatcher import ExactMatcher 
+from proj2Lib.ExactMatcher import ExactMatcherNode
 
 # UPDATE THIS FILE TO IMPLEMENT THE AHO-CORASICK ALGORITHM
 class ACSetMatcher(ExactMatcher):
-	def __init__(self, patterns):
+	def __init__(self, patterns, reportFormat='Found match of pattern in position {0}'):
+		self.root = ExactMatcherNode()
+		self.reportFormat = reportFormat
+		self.patternLengths = [len(x) for x in patterns]
+		self.patternLength = min(self.patternLengths)
+
 		# do initialization here 
 		# don't use super call since that uses preprocessPattern which we don't want to do here
 		self.preprocessPatterns(patterns)
 		
+	def buildKeywordTree(self, patterns):
+		self.root.setFailureLink(self.root)
+		self.root.targetShift = 1
+
+		for pattern in patterns:
+			currentNode = self.root
+			for x in pattern:
+				if currentNode.isMatch(x):
+					currentNode, _ = currentNode.getTransition(x)
+				else:
+					newNode = ExactMatcherNode()
+					newNode.setFailureLink(self.root)
+					newNode.targetShift = currentNode.targetShift - 1
+					currentNode.setChild(x, newNode)
+					currentNode = newNode
+
 	def findFailureLink(self,node,x):
 		# implement algorithm of section 3.4.5
 		# consult KMPMatcher to see how it uses the automata representation 
@@ -17,11 +38,12 @@ class ACSetMatcher(ExactMatcher):
 	def preprocessPatterns(self, patterns):
 		# use findFailureLink here to construct complete keyword tree for
 		# pattern set
-		pass
+		self.buildKeywordTree(patterns)
 
-	def matchTarget(self, target):
-		# For testing purposes, note that this method as defined by ExactMatcher
-		# implements the AC Search Algorithm of Section 3.4.4
-		#
-		# However, for the project you need to implement full AC Search Algorithm of Section 3.4.6
-		pass
+
+	# def matchTarget(self, target):
+	# 	# For testing purposes, note that this method as defined by ExactMatcher
+	# 	# implements the AC Search Algorithm of Section 3.4.4
+	# 	#
+	# 	# However, for the project you need to implement full AC Search Algorithm of Section 3.4.6
+	# 	pass
--- KMPSetMatcher.py
+++ KMPSetMatcher.py
@@ -1,12 +1,13 @@
-from proj2Lib.KMPMatcher import KMPMatcher as KMPMatcher
+from proj2Lib.KMPMatcher import KMPMatcher
 
 # UPDATE THIS CLASS TO IMPLEMENT SET MATCHING WITH THE KMP ALGORITHM
 class KMPSetMatcher(object):
 	def __init__(self, patterns):
-		pass
+		self.matchers = [KMPMatcher(patterns[i], reportFormat = 'Found match of pattern %d at position {0}' % i) for i in xrange(len(patterns))]
 
 	def matchTarget(self, target):
-		pass
+		for matcher in self.matchers:
+			matcher.matchTarget(target)
 		
 	def getTotalTime(self):
 		sum = 0
